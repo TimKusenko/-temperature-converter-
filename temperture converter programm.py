300c0c6a7d4141cf20d4819cb9836a32c8a5ab19
TimKusenko <timkusenko.tk@gmail.com>
2026-06-28 17:22:54 +0200

    Add files via upload

---
+++ temperture converter programm.py
@@ -0,0 +1,12 @@
+
+unit = input("is this temperture in Celsius or Fahrenheit? (C/F): ")
+temp = float(input("Enter the tempeture: "))
+
+if unit == "C": 
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temputure in Fahrenheit is: {temp}° F")
+elif unit == "F":
+    temp = round((temp - 32) * 5/9, 1)
+    print(f"The temeture in Celcius is: {temp}° C")
+else:
+    print(f"{unit} is an invalid unit of measurement")
